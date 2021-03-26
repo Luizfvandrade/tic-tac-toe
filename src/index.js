5016dbbbd2513800bf49213654d2955f1571bb5a
Lzpp3 <luizfelippevieira@outlook.com>
2021-03-26 17:52:22 -0300

    feat(index): changed class Quadrado to a function

--- src/index.js
+++ src/index.js
@@ -2,18 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Quadrado extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
-}
+function Quadrado(props) {
+  return(
+    <button className='square' onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+} 
 
 class Tabuleiro extends React.Component {
   constructor(props) {
