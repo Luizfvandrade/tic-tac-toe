676fa93f98ab51453d0abf29de64eca40a9953af
Lzpp3 <luizfelippevieira@outlook.com>
2021-03-26 17:58:57 -0300

    feat(index): added a method to select a winner

--- src/index.js
+++ src/index.js
@@ -15,13 +15,17 @@ class Tabuleiro extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const quadrados = this.state.squares.slice();
     quadrados[i] = 'X';
-    this.setState({ squares: quadrados })
+    this.setState({ 
+      squares: quadrados,
+      xIsNext: !this.state.xIsNext,
+    })
   }
 
   renderSquare(i) {
@@ -34,7 +38,13 @@ class Tabuleiro extends React.Component {
   }
 
   render() {
-    const status = "Proximo Jogador:";
+    const vencedor = calculateWinner(this.state.squares)
+    let status
+    if (vencedor) {
+      status = 'Vencedor: ' + vencedor
+    } else {
+      status = 'Proximo Jogador: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -74,5 +84,27 @@ class TicTacToe extends React.Component {
     );
   }
 }
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 
 ReactDOM.render(<TicTacToe />, document.getElementById("root"));
